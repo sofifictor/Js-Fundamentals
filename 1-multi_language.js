dc9b73f3f5c7d31b93286f16f0ac07e91a17ee99
sofinuri16 <sofinuri32@gmmail.com>
2025-07-10 23:37:05 +0100

    Add script that prints  3 language lines

--- 1-multi_language.js
+++ 1-multi_language.js
@@ -1,3 +1,1 @@
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log("C is fun"); console.log("Python is cool"); console.log("JavaScript is amazing");

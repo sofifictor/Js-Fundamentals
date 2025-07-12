4d81d3fdeb72adb5fc990421e801ef163c85ded9
sofinuri16 <sofinuri32@gmmail.com>
2025-07-12 23:25:50 +0100

    Fix: correct loop and console.log usage in 7-multi-v.js

--- 7-multi_v.js
+++ 7-multi_v.js
@@ -5,7 +5,9 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }

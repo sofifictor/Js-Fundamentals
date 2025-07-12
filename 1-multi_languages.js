4a275bdcddfccc8303cd807a1f34037301fc715e
sofinuri16 <sofinuri32@gmmail.com>
2025-07-12 10:38:40 +0100

    Fix: restored and corrected 1-multi_laanguages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+    console.log("No arguments");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
+=======
+console.log("C is fun"); console.log("Python is cool"); console.log("JavaScript is amazing");
+>>>>>>> b97fbd1465d65381aa099db5816c5c91610ac90f

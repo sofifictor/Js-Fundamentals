b97fbd1465d65381aa099db5816c5c91610ac90f
sofinuri16 <sofinuri32@gmmail.com>
2025-07-12 09:36:59 +0100

    Add 1-multi_languages.js script

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,1 +1,10 @@
-console.log("C is fun"); console.log("Python is cool"); console.log("JavaScript is amazing");
+// 2-arguments.js
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.log("No arguments");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
+

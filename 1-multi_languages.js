391e97de5a9c1b597c5711be7df382aade5f6a6c
sofinuri16 <sofinuri32@gmmail.com>
2025-07-13 16:28:53 +0100

    Fix: remove 'No argument' output

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,20 +1,5 @@
-#!/usr/bin/node
-
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-if (arg1 === undefined) {
-    console.log('No argument');
-} else if (arg2 === undefined) {
-    console.log(arg1);
-} else {
-    console.log(`${arg1} ${arg2}`);
-}
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
 
 

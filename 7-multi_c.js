3c4f6886cd738f6ae6a49aca5ce150a78ba56a8c
sofinuri16 <sofinuri32@gmmail.com>
2025-07-13 16:51:28 +0100

    Add 7-multi_c.js script to print x times 'C is fun'

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
 const x = parseInt(process.argv[2]);
-
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
-  }
+  for (let i = 0; i < x; i++) console.log('C is fun');
 }
